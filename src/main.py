a953d17c6a752f5a40c83bd36467b27a0500a5bf
Aziz Shaik <azizs@vt.edu>
2025-11-24 18:49:39 -0500

    Fixed main errors and added proper calls to infragraph

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from utils import convert_fires_sql_to_csv
 from wildfire_graph import WildfireGraph
-from infra_graph import build_infra_graph
+from infra_graph import InfraGraph
 # from graph_integration import analyze_overlay
 # from clustering import run_clustering
 from config import WILDFIRE_CSV_PATH, POWER_LINES_CSV_PATH, POWER_PLANTS_CSV_PATH, SUBSTATIONS_CSV_PATH
@@ -8,11 +8,11 @@ from config import WILDFIRE_CSV_PATH, POWER_LINES_CSV_PATH, POWER_PLANTS_CSV_PAT
 def main():
     convert_fires_sql_to_csv()
 
-    wildfire_graph = WildfireGraph("data/fires.csv", 5, 24)
-    wf_graph, metrics = wildfire_graph.build_graph()
+    wildfire_graph = WildfireGraph(WILDFIRE_CSV_PATH, 5, 24)
+    wf_graph, wf_metrics = wildfire_graph.build_graph()
 
-    infra_graph = build_infra_graph(POWER_LINES_CSV_PATH, POWER_PLANTS_CSV_PATH, SUBSTATIONS_CSV_PATH)
-
+    infra_graph = InfraGraph(POWER_LINES_CSV_PATH, POWER_PLANTS_CSV_PATH, SUBSTATIONS_CSV_PATH)
+    i_graph, i_metrics = infra_graph.build_graph()
 
 
 if __name__ == "__main__":
