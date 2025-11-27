a7dfc8c1d841ee52ba54825daf557d626f1d3f74
Aziz Shaik <azizs@vt.edu>
2025-11-27 10:06:27 -0500

    Updated wildfire_graph to use new data and plot new graph

--- src/main.py
+++ src/main.py
@@ -3,17 +3,14 @@ from wildfire_graph import WildfireGraph
 from infra_graph import InfraGraph
 # from graph_integration import analyze_overlay
 # from clustering import run_clustering
-from config import WILDFIRE_CSV_PATH, POWER_LINES_CSV_PATH, POWER_PLANTS_CSV_PATH, SUBSTATIONS_CSV_PATH
+from config import POWER_LINES_CSV_PATH, POWER_PLANTS_CSV_PATH, SUBSTATIONS_CSV_PATH
 
 def main():
-    convert_fires_sql_to_csv()
-
-    wildfire_graph = WildfireGraph(WILDFIRE_CSV_PATH, 5, 24)
+    wildfire_graph = WildfireGraph()
     wf_graph, wf_metrics = wildfire_graph.build_graph()
 
     infra_graph = InfraGraph(POWER_LINES_CSV_PATH, POWER_PLANTS_CSV_PATH, SUBSTATIONS_CSV_PATH)
     i_graph, i_metrics = infra_graph.build_graph()
-
 
 if __name__ == "__main__":
     main()
--- src/wildfire_graph.py
+++ src/wildfire_graph.py
@@ -3,90 +3,60 @@ import pandas as pd
 from geopy.distance import geodesic
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from scipy.spatial import KDTree
-import pickle
 import contextily as ctx
 import geopandas as gpd
-from config import WILDFIRE_PKL_PATH, WILDFIRE_SHP_PATH, WILDFIRE_PERIMS_SHP_PATH
+from config import WILDFIRE_SHP_PATH, WILDFIRE_PERIMS_SHP_PATH
 
 class WildfireGraph:
-    def __init__(self, fires_csv, spatial_dist, time_dist):
-        self.fires_csv_path = fires_csv
-        self.spatial_dist = spatial_dist
-        self.time_dist = time_dist
+    def __init__(self):
         self.graph = nx.Graph()
         self.metrics = {}
 
     def build_graph(self):
         mtbs_pts = gpd.read_file(WILDFIRE_SHP_PATH)
 
-        # print(mtbs.head())
-        # print(mtbs.columns)
-        # mtbs.plot(markersize=2, figsize=(10, 10), alpha=0.6)
-        # plt.savefig("wildfire_graph_mtbs.png", dpi=300)
-
         mtbs_perims = gpd.read_file(WILDFIRE_PERIMS_SHP_PATH)
-        print(f"all_size: {len(mtbs_perims)}")
-        mtbs_perims_ca = mtbs_perims[mtbs_perims["Event_ID"].str.startswith("CA")]
+        mtbs_perims_ca = mtbs_perims[mtbs_perims["Event_ID"].str.startswith("CA")].copy()
         print(f"ca_size: {len(mtbs_perims_ca)}")
-        # print(mtbs_perims_ca.head())
-
 
         # Reproject for basemap
-        mtbs_perims_web = mtbs_perims_ca.to_crs(epsg=3857)
+        mtbs_perims_ca = mtbs_perims_ca.to_crs(epsg=3857)
+        mtbs_perims_ca["centroid"] = mtbs_perims_ca.geometry.centroid
 
-        # Plot polygons colored by severity
-        ax = mtbs_perims_web.plot(column="BurnBndAc", cmap="OrRd", alpha=0.5, figsize=(12, 12))
+        # Save the plot that just has the perims with no edges
+        ax = mtbs_perims_ca.plot(column="BurnBndAc", cmap="OrRd", alpha=0.5, figsize=(12, 12))
         ctx.add_basemap(ax, source=ctx.providers.CartoDB.DarkMatter)
         plt.savefig("wildfire_graph_mtbs.png", dpi=300)
-        # cols_to_use = ["FOD_ID", "LATITUDE", "LONGITUDE", "DISCOVERY_DATE", "FIRE_SIZE",
-        #     "FIRE_SIZE_CLASS", "NWCG_CAUSE_CLASSIFICATION", "STATE", "OWNER_DESCR"]
-        # fires_df = self._preprocessing(pd.read_csv(self.fires_csv_path, usecols=cols_to_use))
 
-        # print("Building wildfire graph...")
+        # Find pairs of wildfires that overlap and remove self-joins
+        pairs = gpd.sjoin(mtbs_perims_ca, mtbs_perims_ca)
+        pairs = pairs[pairs["Event_ID_left"] != pairs["Event_ID_right"]]
         
-        # for _, row in tqdm(fires_df.iterrows(), total=len(fires_df), desc="Adding nodes"):
-        #     self.graph.add_node(row['FOD_ID'], latitude=row['LATITUDE'], longitude=row['LONGITUDE'],
-        #                         date=row['DISCOVERY_DATE'], fire_size=row['FIRE_SIZE'], 
-        #                         fire_class=row['FIRE_SIZE_CLASS'], cause=row['NWCG_CAUSE_CLASSIFICATION'],
-        #                         state=row['STATE'], owner=row['OWNER_DESCR'])
+        # Make the nodes just based on all of the fire regions
+        for _, row in tqdm(mtbs_perims_ca.iterrows(), total=len(mtbs_perims_ca), desc="Adding nodes"):
+            self.graph.add_node(row["Event_ID"], size=row["BurnBndAc"], severity=row["High_T"])
+        
+        # Make the edges based on spatial overlaps
+        for _, row in tqdm(pairs.iterrows(), total=len(pairs), desc="Adding edges"):
+            self.graph.add_edge(row["Event_ID_left"], row["Event_ID_right"])
 
-        # coords = fires_df[['LATITUDE', 'LONGITUDE']].to_numpy()
-        # kd_tree = KDTree(coords)
+        # Plot the wildfires. Using BurnBndAc to color based on severity for now
+        fig, ax = plt.subplots(figsize=(12, 12))
+        mtbs_perims_ca.plot(column="BurnBndAc", cmap="OrRd", alpha=0.5, ax=ax)
 
-        # for idx, row in tqdm(fires_df.iterrows(), total=len(fires_df), desc="Building edges"):
-        #     coord = (row['LATITUDE'], row['LONGITUDE'])
-        #     radius_deg = self.spatial_dist / 69.0
-        #     neighbor_idxs = kd_tree.query_ball_point([row['LATITUDE'], row['LONGITUDE']], r=radius_deg)
+        # Plot edges between centroids
+        for u, v in self.graph.edges():
+            c1 = mtbs_perims_ca.loc[mtbs_perims_ca["Event_ID"] == u, "centroid"].values[0]
+            c2 = mtbs_perims_ca.loc[mtbs_perims_ca["Event_ID"] == v, "centroid"].values[0]
+            xs = [c1.x, c2.x]
+            ys = [c1.y, c2.y]
+            ax.plot(xs, ys, color="blue", linewidth=0.5, alpha=0.5)
 
-        #     for n_idx in neighbor_idxs:
-        #         if n_idx <= idx:  # avoid duplicates
-        #             continue
-        #         neighbor = fires_df.iloc[n_idx]
-        #         time_diff = abs((row['DISCOVERY_DATE'] - neighbor['DISCOVERY_DATE']).total_seconds()) / 3600
-        #         if time_diff <= self.time_dist:
-        #             dist_miles = geodesic(coord, (neighbor['LATITUDE'], neighbor['LONGITUDE'])).miles
-        #             self.graph.add_edge(row['FOD_ID'], neighbor['FOD_ID'],
-        #                                 spatial_dist=dist_miles, time_diff=time_diff)
-    
-        # print("Wildfire Graph Info:")
-        # print(f"Nodes: {self.graph.number_of_nodes()}")
-        # print(f"Edges: {self.graph.number_of_edges()}")
-        # print("Number of clusters:", nx.number_connected_components(self.graph))
-        # # print("Cluster sizes:", [len(c) for c in list(nx.connected_components(self.graph))])
+        # Add dark basemap for new plot
+        ctx.add_basemap(ax, source=ctx.providers.CartoDB.DarkMatter)
+        plt.savefig("wildfire_graph_edges.png", dpi=300)
 
-        # with open(WILDFIRE_PKL_PATH, "wb") as f:
-        #     pickle.dump(self.graph, f)
-
-        # pos = {node:(data['longitude'], data['latitude']) for node, data in self.graph.nodes(data=True)}
-
-        # nx.draw(self.graph, pos, node_size=0.001, alpha=0.6, with_labels=False)
-        # plt.xlabel("Longitude")
-        # plt.ylabel("Latitude")
-        # plt.show()
-        # plt.savefig("wildfire_graph.png", dpi=300)
-
-        return self.graph, self._compute_metrics
+        return self.graph, self._compute_metrics()
 
     def _preprocessing(self, df):
         df['DISCOVERY_DATE'] = pd.to_datetime(df['DISCOVERY_DATE'], errors='coerce')
@@ -101,10 +71,9 @@ class WildfireGraph:
         self.metrics['num_clusters'] = nx.number_connected_components(self.graph)
         self.metrics['clusters'] = list(nx.connected_components(self.graph))
 
-        print("Degree centrality:", self.metrics['degree_centrality'])
-        print("Clustering:", self.metrics['clustering'])
-        print("Betweenness:", self.metrics['betweenness'])
-        print("Number of clusters:", self.metrics['num_clusters'])
-        print("Cluster sizes:", [len(c) for c in self.metrics['clusters']])
-
-        return
+        # print("Degree centrality:", self.metrics['degree_centrality'])
+        # print("Clustering:", self.metrics['clustering'])
+        # print("Betweenness:", self.metrics['betweenness'])
+        # print("Number of clusters:", self.metrics['num_clusters'])
+        # print("Cluster sizes:", [len(c) for c in self.metrics['clusters']])
+        return self.metrics
